2ff5f7e66ba379ce3512c22f970469f0be19a403
vamsi Gone <108540645+vamsiGone@users.noreply.github.com>
2022-11-23 20:31:56 +0530

    Update Abbreviate_two_word_name.cs

--- Abbreviate_two_word_name.cs
+++ Abbreviate_two_word_name.cs
@@ -1,6 +1,6 @@
 public static string AbbrevName(string name)
   {
-    string[] names=name.Split(" ");
+   string[] names=name.ToUpper().Split(" ");
     string first=names[0];
     string sec=names[1];
     return first[0]+"."+sec[0];
