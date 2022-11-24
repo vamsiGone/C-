e9bd6a093b7bb5239de3ecdc58a4034825b17ead
vamsi Gone <108540645+vamsiGone@users.noreply.github.com>
2022-11-24 19:58:16 +0530

    Update Cockroach_Speed.cs

--- Cockroach_Speed.cs
+++ Cockroach_Speed.cs
@@ -1,7 +1,8 @@
 public static int CockroachSpeed(double x)
     {
       //#Happy Coding! ^_^
-      double MPS = x * 0.277778 *100;
+       double MPS = Math.Floor(x * 0.277778 *100);
+      
       int s=Convert.ToInt32(MPS);
       return s;
     }
